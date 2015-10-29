1692ed4796e7fb1a2f5850422d71a0796921c582
Luminarys <kizunanohikari@gmail.com>
2015-10-29 11:08:22 -0500

    Disable CSRF protection on file serving routes

--- web/router.ex
+++ web/router.ex
@@ -5,8 +5,11 @@ defmodule Eientei.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
     plug :put_secure_browser_headers
+  end
+
+  pipeline :csrf do
+    plug :protect_from_forgery
   end
 
   pipeline :api do
@@ -14,12 +17,17 @@ defmodule Eientei.Router do
   end
 
   scope "/", Eientei do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :csrf]
 
     get "/", PageController, :index
     get "/faq", PageController, :faq
     get "/contact", PageController, :contact
     get "/tools", PageController, :tools
+  end
+
+  scope "/", Eientei do
+    pipe_through [:browser]
+
     # Let's not break compat quite yet
     get "/:file", PageController, :file
     get "/f/:file", PageController, :file
