cf2dea50eeab6ee3db3984ae415dece25d89a142
Luminarys <kizunanohikari@gmail.com>
2015-11-14 15:03:18 -0600

    Remove arithmatic from attributes

--- lib/eientei/endpoint.ex
+++ lib/eientei/endpoint.ex
@@ -30,11 +30,12 @@ defmodule Eientei.Endpoint do
     plug Plug.CloudFlare
   end
 
+  max_size = @max_upload_size * 1000 * 1000
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Poison,
-    length: @max_upload_size * 1000 * 1000
+    length: max_size
 
   plug Plug.MethodOverride
   plug Plug.Head
