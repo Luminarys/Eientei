d22c6026c1e807938bd0bdde4356e6d857f85c2b
Luminarys <kizunanohikari@gmail.com>
2015-11-17 23:00:05 -0600

    Refactor upload and file viewing code
    A large portion of the upload controller and file viewing controller
    code has been refactored, This was done to make the code more idiomatic
    as well as improve its general readability. Large functions were broken
    up into smaller composable ones, and duplicate code was reduced as much
    as possible.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,6 +3,10 @@ defmodule Eientei.PageController do
 
   @service_domain Application.get_env(:eientei, :service_domain)
 
+  @use_fallback Application.get_env(:eientei, :fallback_service)
+  @fallback_url Application.get_env(:eientei, :fallback_service_url)
+  @max_cache_size Application.get_env(:eientei, :max_cache_size)
+
   def index(conn, _params) do
     render conn, "index.html"
   end
@@ -23,81 +27,73 @@ defmodule Eientei.PageController do
     render conn, "tools.html"
   end
 
-  @use_fallback Application.get_env(:eientei, :fallback_service)
-  @fallback_url Application.get_env(:eientei, :fallback_service_url)
-  @max_cache_size Application.get_env(:eientei, :max_cache_size)
-
-  def file(conn, %{"file" => file}) do
+  def file(conn, %{"file" => filename}) do
     require Pipe
-
-    resp = Pipe.pipe_matching val, {:ok, val},
-    {:ok, file}
+    resp =Pipe.pipe_matching val, {:ok, val},
+    {:ok, filename}
     |> check_cache
     |> check_file_loc
     |> check_db
     |> check_fallback
 
-    case resp do
-      :redirect ->
-        redirect conn, external: "#{@fallback_url}/#{file}"
-      {:cached, val} ->
-        conn
-        |> put_resp_content_type(Plug.MIME.path(file))
-        |> send_resp(200, val)
-      {:file, loc} ->
-        max = @max_cache_size
-        Task.start(fn ->
-          require Logger
-          ets = ConCache.ets(:file_cache)
-          size = :ets.info(ets) |> Keyword.get(:size)
-          cond do
-            size >= max ->
-              # High quality algorithms right here
-              :random.seed(:os.timestamp)
-              {item, _} = Enum.random(ets |> :ets.tab2list)
-              Logger.log :debug, "Cache is too big, deleting #{item} and adding #{file}"
-              ConCache.delete(:file_cache, item)
-              ConCache.put(:file_cache, file, File.read! loc)
-            size < max ->
-              Logger.log :debug, "Adding #{file} to the cache"
-              ConCache.put(:file_cache, file, File.read! loc)
-          end
-        end)
-
-         conn
-         |> put_resp_content_type(Plug.MIME.path(file))
-         |> send_file(200, loc)
-      :no_file ->
-        conn
-        |> put_status(404)
-        |> put_flash(:error, "The file you tried to view does not exist!")
-        |> render "index.html"
-    end
+    send_file_response(resp, conn, filename)
   end
 
-  defp check_cache(file) do
-    case ConCache.get(:file_cache, file) do
-      nil -> {:ok, file}
+  defp send_file_response(:redirect, conn, filename) do
+    redirect conn, external: "#{@fallback_url}/#{filename}"
+  end
+  defp send_file_response({:cached, data}, conn, filename) do
+    conn
+    |> put_resp_content_type(Plug.MIME.path(filename))
+    |> send_resp(200, data)
+  end
+  defp send_file_response({:file, filename, path}, conn, filename) do
+    Task.start(fn -> cache_file(filename, path) end)
+    conn
+    |> put_resp_content_type(Plug.MIME.path(path))
+    |> send_file(200, path)
+  end
+  defp send_file_response(:no_file, conn, _file) do
+    conn
+    |> put_status(404)
+    |> put_flash(:error, "The file you tried to view does not exist!")
+    |> render "index.html"
+  end
+
+  defp cache_file(filename, path) do
+    ets = ConCache.ets(:file_cache)
+    size = :ets.info(ets) |> Keyword.get(:size)
+    if size >= @max_cache_size do
+        :random.seed(:os.timestamp)
+        {item, _} = Enum.random(ets |> :ets.tab2list)
+        ConCache.delete(:file_cache, item)
+    end
+    ConCache.put(:file_cache, filename, File.read! path)
+  end
+
+  defp check_cache(filename) do
+    case ConCache.get(:file_cache, filename) do
+      nil -> {:ok, filename}
       val -> {:cached, val}
     end
   end
 
-  defp check_file_loc(file) do
-    case File.exists?("files/" <> file) do
-       true -> {:file, "files/" <> file}
-       false -> {:ok, file}
+  defp check_file_loc(filename) do
+    case File.exists?("files/" <> filename) do
+       true -> {:file, filename, "files/" <> filename}
+       false -> {:ok, filename}
     end
   end
 
-  defp check_db(file) do
-    case Eientei.Repo.get_by(Eientei.Upload, name: file) do
-      nil -> {:ok, file}
-      %{location: location} -> {:file, location}
+  defp check_db(filename) do
+    case Eientei.Repo.get_by(Eientei.Upload, name: filename) do
+      nil -> {:ok, filename}
+      %{location: path} -> {:file, filename, path}
     end
   end
 
-  defp check_fallback(file) when @use_fallback do
-    {:ok, fallback_resp} = HTTPoison.get("#{@fallback_url}/#{file}")
+  defp check_fallback(filename) when @use_fallback do
+    {:ok, fallback_resp} = HTTPoison.get("#{@fallback_url}/#{filename}")
       case fallback_resp do
         %{status_code: 200} -> :redirect
         %{status_code: 301} -> :redirect
--- web/controllers/upload_controller.ex
+++ web/controllers/upload_controller.ex
@@ -8,6 +8,7 @@ defmodule Eientei.UploadController do
   @service_url Application.get_env(:eientei, :service_url)
 
   @illegal_exts [".ade", ".adp", ".bat", ".chm", ".cmd", ".com", ".cpl", ".exe", ".hta", ".ins", ".isp", ".jse", ".lib", ".lnk", ".mde", ".msc", ".msp", ".mst", ".pif", ".scr", ".sct", ".shb", ".sys", ".vb", ".vbe", ".vbs", ".vxd", ".wsc", ".wsf", ".wsh"]
+  @name_len 6
 
   def upload(conn, %{"file" => file}) when is_list(file) do
     json conn, failure("You can only send individual files using the file param!")
@@ -20,7 +21,7 @@ defmodule Eientei.UploadController do
     cfiles = Enum.map(files, &({&1, conn}))
     json conn, %{"files" => Enum.map(cfiles, &upload_file/1)}
   end
-  def upload(conn, %{"files" => files}) do
+  def upload(conn, %{"files" => _files}) do
     json conn, failure("You can only send a list of files with the files param!")
   end
 
@@ -29,28 +30,35 @@ defmodule Eientei.UploadController do
     json conn, failure("Please send files using the file or files key!")
   end
 
-  defp upload_file({file, conn}) do
-    # Randomly generate unused file name
-    # Return filename as url, queue an async check
-    # Check hashes file, sees if hash is in db
-    # and modifies file location if necessary,
-    # removing any duplicates
-    use Pipe
+  defp failure(reason), do: %{"success" => false, "reason" => reason}
+  defp file_success(name, old_name), do: %{"url" => "#{@service_url}/f/#{name}", "name" => old_name, "success" => true}
+  defp file_failure(name, reason), do: %{"name" => name, "success" => false, "reason" => reason}
 
-    name_len = 6
+  @doc """
+  Perform size, rate, and file type checks, on a file,
+  then generates an unused file name, moves the file,
+  and commits the transaction to the database.
+  """
+  defp upload_file({file, conn}) do
+    res = check_file({file, conn})
+    case res do
+      %{"success" => false} -> res
+      file ->
+        file
+        |> gen_name
+        |> move_file
+        |> generate_db_entry
+    end
+  end
+
+  defp check_file({file, conn}) do
+    use Pipe
     Pipe.pipe_matching val, {:ok, val},
-    {:ok, file}
+    file
     |> check_file_size
     |> check_magic_number
     |> check_data_rate(conn)
-    |> gen_name(name_len)
-    |> move_file
-    |> generate_db_entry
   end
-
-  defp file_success(name, old_name), do: %{"url" => "#{@service_url}/f/#{name}", "name" => old_name, "success" => true}
-  defp file_failure(name, reason), do: %{"name" => name, "success" => false, "reason" => reason}
-  defp failure(reason), do: %{"success" => false, "reason" => reason}
 
   defp check_file_size(file) do
     %{size: size} = File.stat! file.path
@@ -61,11 +69,22 @@ defmodule Eientei.UploadController do
   end
 
   defp check_magic_number(file) do
-    case is_exe(hd(Enum.take(File.stream!(file.path),1))) do
+    b = get_init_bytes(file.path)
+    case is_exe(b) do
       false -> {:ok, file}
       true -> file_failure file.filename, "Exe's are not allowed!"
     end
   end
+
+  defp get_init_bytes(path) do
+    path
+    |> File.stream!
+    |> Enum.take(1)
+    |> hd
+  end
+
+  defp is_exe(<<77::8, 90::8, _rest::binary>>), do: true
+  defp is_exe(_data), do: false
 
   defp check_data_rate(file, conn) do
     %{size: size} = File.stat! file.path
@@ -75,17 +94,26 @@ defmodule Eientei.UploadController do
     end
   end
 
-  defp gen_name(file, length) do
-    name = :crypto.rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
+  defp gen_name(file) do
+    name = get_rand_chars(@name_len)
     loc = "files/" <> name
     case File.exists?(loc) do
-      true -> gen_name(file, length)
-      false ->
-        case Eientei.Repo.get_by(Eientei.Upload, name: name) do
-          nil -> {:ok, {file, name}}
-          _ -> gen_name(file, length)
-        end
+      true -> gen_name(file)
+      false -> check_db_names(file, name)
     end
+  end
+
+  defp check_db_names(file, name) do
+    case Eientei.Repo.get_by(Eientei.Upload, name: name) do
+      nil -> {file, name}
+      _ -> gen_name(file)
+    end
+  end
+
+  defp get_rand_chars(length) do
+    :crypto.rand_bytes(length)
+    |> Base.url_encode64
+    |> binary_part(0, length)
   end
 
   defp move_file({file, new_name}) do
@@ -95,51 +123,16 @@ defmodule Eientei.UploadController do
     new_path = "files/" <> name
     # Move file, since Elixir seems to not provide an equivalent
     System.cmd("mv", [path, new_path])
-    {:ok, {file, name, new_path, old_name}}
+    {name, new_path, old_name}
   end
 
-  defp generate_db_entry({_file, full_name, loc, orig_name}) do
-    import Ecto.Query, only: [from: 2]
+  defp generate_db_entry({name, loc, orig_name}) do
     r = File.read! loc
-    ConCache.put(:file_cache, full_name, r)
     hash = md5(r)
-    # Perhaps find a better place to do this
-    max = @max_cache_size
-    Task.start(fn ->
-      ets = ConCache.ets(:file_cache)
-      size = :ets.info(ets) |> Keyword.get(:size)
-      require Logger
-      cond do
-        size >= max ->
-          # High quality algorithms right here
-          :random.seed(:os.timestamp)
-          {item, _} = Enum.random(ets |> :ets.tab2list)
-          Logger.log :debug, "Cache is too big, deleting #{item} and adding #{full_name}"
-          ConCache.delete(:file_cache, item)
-          ConCache.put(:file_cache, full_name, r)
-        size < max ->
-          Logger.log :debug, "Adding #{full_name} to the cache"
-          ConCache.put(:file_cache, full_name, r)
-      end
-    end)
+    Task.start(fn -> update_cache(name, r) end)
     %{size: size} = File.stat! loc
-
-    query = from u in Eientei.Upload,
-        where: u.hash == ^hash,
-        select: u.location,
-        limit: 1
-
-    case Eientei.Repo.one(query) do
-      nil ->
-        changeset = Eientei.Upload.changeset(%Eientei.Upload{}, %{:name => full_name, :location => loc, :hash => hash, :filename => orig_name, :size => size})
-        {:ok, _user} = Eientei.Repo.insert(changeset)
-        file_success full_name, orig_name
-      location ->
-        File.rm! loc
-        changeset = Eientei.Upload.changeset(%Eientei.Upload{}, %{:name => full_name, :location => location, :hash => hash, :filename => orig_name, :size => size})
-        {:ok, _user} = Eientei.Repo.insert(changeset)
-        file_success full_name, orig_name
-    end
+    new_loc = remove_duplicate_file(hash, loc)
+    add_db_entry(name, new_loc, hash, orig_name, size)
   end
 
   defp md5(data) do
@@ -150,6 +143,35 @@ defmodule Eientei.UploadController do
     |> :erlang.list_to_bitstring
   end
 
-  defp is_exe(<<77::8, 90::8, _rest::binary>>), do: true
-  defp is_exe(_data), do: false
+  defp update_cache(file, name) do
+    ets = ConCache.ets(:file_cache)
+    size = :ets.info(ets) |> Keyword.get(:size)
+    if size >= @max_cache_size do
+      # High quality algorithms right here
+      :random.seed(:os.timestamp)
+      {item, _} = Enum.random(ets |> :ets.tab2list)
+      ConCache.delete(:file_cache, item)
+    end
+    ConCache.put(:file_cache, name, file)
+  end
+
+  defp remove_duplicate_file(hash, location) do
+    import Ecto.Query, only: [from: 2]
+    query = from u in Eientei.Upload,
+        where: u.hash == ^hash,
+        select: u.location,
+        limit: 1
+    case Eientei.Repo.one(query) do
+      nil -> location
+      existing_location ->
+        File.rm! location
+        existing_location
+    end
+  end
+
+  defp add_db_entry(full_name, location, hash, orig_name, size) do
+    changeset = Eientei.Upload.changeset(%Eientei.Upload{}, %{:name => full_name, :location => location, :hash => hash, :filename => orig_name, :size => size})
+    {:ok, _user} = Eientei.Repo.insert(changeset)
+    file_success full_name, orig_name
+  end
 end
