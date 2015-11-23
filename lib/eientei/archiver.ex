10ab4ed53a0d385b025b4e3892ae210a1256944e
Luminarys <kizunanohikari@gmail.com>
2015-11-23 12:21:10 -0600

    Add in IA S3 archive rate limit checker in archiving task

--- lib/eientei/archiver.ex
+++ lib/eientei/archiver.ex
@@ -4,7 +4,14 @@ defmodule Eientei.Archiver do
   @make_bucket {"x-amz-auto-make-bucket", "1"}
   @collection {"x-archive-meta01-collection", "opensource_media"}
   @media_type {"x-archive-meta-mediatype", "web"}
+  @ia_sponsor {"x-archive-meta-sponsor", Application.get_env(:einetei, :ia_sponsor)}
+
+  @ia_access Application.get_env(:eientei, :ia_access)
+  @ia_secret Application.get_env(:eientei, :ia_secret)
+  @ia_auth {"authorization", "LOW #{@ia_access}:#{@ia_secret}"}
+
   @service Application.get_env(:eientei, :ia_service_name)
+  @bucket_name "#{@service}_archive"
 
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: Archiver)
@@ -40,21 +47,22 @@ defmodule Eientei.Archiver do
     query = from u in Eientei.Upload,
         where: u.name == ^name
     file = Eientei.Repo.one query
-    {:ok, _file} = Eientei.Repo.update(%{file | archived_url: "https://archive.org/download/#{@service}_archive/#{name}"})
+    {:ok, _file} = Eientei.Repo.update(%{file | archived_url: "https://archive.org/download/#{@bucket_name}/#{name}"})
   end
 
   defp archive_file(name, location) do
+    check_rate_limit
     access = Application.get_env(:eientei, :ia_access)
     secret = Application.get_env(:eientei, :ia_secret)
     headers = [
       @make_bucket,
       @collection,
       @media_type,
-      {"x-archive-meta-sponsor", Application.get_env(:einetei, :ia_sponsor)},
-      {"authorization", "LOW #{access}:#{secret}"}
+      @ia_sponsor,
+      @ia_auth,
     ]
     try do
-      {:ok, resp} = HTTPoison.put("http://s3.us.archive.org/#{@service}_archive/#{name}",
+      {:ok, resp} = HTTPoison.put("http://s3.us.archive.org/#{@bucket_name}/#{name}",
                     {:file, "#{location}"},
                     headers)
       case resp do
@@ -63,7 +71,7 @@ defmodule Eientei.Archiver do
           # Try sending with no extension
           # If it fails then log an error
           new_name = Path.basename(name, Path.extension(name))
-          {:ok, new_resp} = HTTPoison.put("http://s3.us.archive.org/#{@service}_archive/#{new_name}",
+          {:ok, new_resp} = HTTPoison.put("http://s3.us.archive.org/#{@bucket_name}/#{new_name}",
                     {:file, "#{location}"},
                     headers)
           case new_resp do
@@ -76,6 +84,17 @@ defmodule Eientei.Archiver do
         # Catches timeout errors
         GenServer.cast(Archiver, {:archive, name, location})
         :ok
+    end
+  end
+
+  defp check_rate_limit() do
+    {:ok, %{:body => body}} = HTTPoison.get("http://s3.us.archive.org/?check_limit=1&bucket=#{@bucket_name}&accesskey=#{@ia_access}")
+    %{"over_limit" => over_limit} = body |> Poison.decode!
+    case over_limit do
+      0 -> :ok
+      1 ->
+        :timer.sleep(10000)
+        check_rate_limit
     end
   end
 end
