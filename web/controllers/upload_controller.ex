bdccb0020dd9442b2aa2498c930a0b6b14ee2bc1
Les De Ridder <les@lesderid.net>
2017-05-28 04:22:03 +0200

    Upgrade to Phoenix 1.2.4

--- web/controllers/upload_controller.ex
+++ web/controllers/upload_controller.ex
@@ -111,7 +111,7 @@ defmodule Eientei.UploadController do
   end
 
   defp get_rand_chars(length) do
-    :crypto.rand_bytes(length)
+    :crypto.strong_rand_bytes(length)
     |> Base.url_encode64
     |> binary_part(0, length)
   end
